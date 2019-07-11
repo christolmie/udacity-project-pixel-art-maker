ceae68e6d5d3872ac43d66dcd770187cc6eb7cd4
Chris Tolmie <chris.tolmie@bigpond.com>
2019-07-11 10:18:21 +1000

    Updates following review. Expanded comments to add clarity.

--- designs.js
+++ designs.js
@@ -1,57 +1,92 @@
 // Select color input
-var colorPicker = document.getElementById('colorPicker');
-var color = colorPicker.value;
+// This is used to color the background of the grid cells
+const colorPicker = document.getElementById('colorPicker');
+let color = colorPicker.value;
 
 colorPicker.addEventListener('change', function() {
     color = colorPicker.value;
 });
 
 // Select size input
-var inputHeight = document.getElementById('inputHeight');
-var inputWidth = document.getElementById('inputWidth');
-var gridHeight = inputHeight.value
-var gridWidth = inputWidth.value
+// These values gridHeight and gridWidth are used to make the grid
+// inputHeight and inputWeight are used to attache the event listeners
+// gridHeight and gridWidth are used by makeGrid() to build the grid
+const inputHeight = document.getElementById('inputHeight');
+const inputWidth = document.getElementById('inputWidth');
+let gridHeight = inputHeight.value
+let gridWidth = inputWidth.value
 
 // Select the submit button
-var submit = document.getElementById('sizePicker');
+// This is used to attach an eventListener to the submit button
+const submit = document.getElementById('sizePicker');
 
-// Event listener for changes to the Grid Height
+// Event listener that listens for changes to the Grid Height
 // Read the new height and redraw the grid
+// Every time the height of the grid is changed, we redraw the grid
+// to clear it and set it to the new size
 inputHeight.addEventListener('change', function () {
     gridHeight = inputHeight.value
-    makeGrid(gridHeight, gridWidth)
+    makeGrid(gridHeight, gridWidth, color)
 })
 
 // Event listener for changes to the Grid Width
 // Read the new width and redraw the grid
+// Same as for the inputHeight. We need to redraw the grid every time
+// the width of the grid is changed.
 inputWidth.addEventListener('change', function () {
     gridWidth = inputWidth.value
-    makeGrid(gridHeight, gridWidth)
+    makeGrid(gridHeight, gridWidth, color)
 })
 
-// When size is submitted by the user, call makeGrid()
-// Prevent the default behaviour and redraw the grid
-submit.addEventListener('click', function(event) {
+// When size is submitted by the user, call makeGrid() to clear the grid
+// We need to prevent the default behaviour so that the whole page does not
+// get reset.
+submit.addEventListener('submit', function(event) {
     event.preventDefault();
-    makeGrid(gridHeight, gridWidth);
+    makeGrid(gridHeight, gridWidth, color);
 })
 
 
-// Your code goes here!
+/*
+* @description Build a grid on the page as a table of cells.
+* This doesn't add the table, but populates an existing table with cells.
+* Each cell in the grid has an event listener attached
+* that changes the background color of the cell to the value that has been
+* selected by the colorPicker.
+*
+* @param {integer} gridHeight - The number of rows in the grid
+* @param {integer} gridWidth - The nimber of columns in the grid
+* #param {string} color - The color to associate with the background color
+*                         EventListener
+*/
 
-function makeGrid(height, width) {
-    // Fidn the table element
-    table = document.getElementById('pixelCanvas');
+function makeGrid(height, width, color) {
+    // Find the table element
+    const table = document.getElementById('pixelCanvas');
+    const maxSize = 50;
 
-    // Remove any existing child elements in the table element
+    // Limit check to prevent the page from attempting to draw a gird that
+    // exceeds the max dimensions set by the input tags in the index.html
+    // file.
+    if ( height > 50 ) {
+        height = 50;
+    }
+
+    if ( width > 50 ) {
+        width = 50;
+    }
+    // Remove any existing cells in the table element so that
+    // a new grid can be build.
+    // Removing each child element in a while loop was chosen because it
+    // is faster than using Element.innerHTML
     while (table.firstChild) {
         table.removeChild(table.firstChild)
     }
 
     // Build the table
-    for (row = 0; row < gridHeight; row++) {
+    for (let row = 0; row < height; row++) {
         newRow = table.insertRow(-1);
-        for (col = 0; col < gridWidth; col++) {
+        for (let col = 0; col < width; col++) {
             newCell = newRow.insertCell(col);
             }
         }
